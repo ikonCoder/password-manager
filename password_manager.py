6bbbb2342ba5aabde029e6bf22ca0d251171dfbd
Matthew Joseph <matthew.j.joe@gmail.com>
2021-02-17 00:05:03 -0500

    Initial script that prompts the main password
    The main password is currently set in PostgreSQL and is the only password required to access all the other usernames and passwords.

--- password_manager.py
+++ password_manager.py
@@ -1,5 +1,5 @@
 from secret import get_master_pwd
-from menu import mainMenu
+from menu import mainMenu, create, find_accounts, find
 
 
 secret = get_master_pwd()
@@ -10,7 +10,15 @@ def runProgram():
 
     if passw == secret:
         print('You\'re in')
-        mainMenu()
+        menuChoice = mainMenu()
+        if menuChoice == '1':
+            create()
+        elif menuChoice == '2':
+            find_accounts()
+        elif menuChoice == '3':
+            find()
+        elif menuChoice == 'Q':
+            exit()
     else:
         choice = input('No luck...would you link to try again? Type YES or NO. ')
         if choice == 'YES':
